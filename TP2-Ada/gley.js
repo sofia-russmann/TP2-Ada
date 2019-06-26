3f485c3c096e3bae4e1cef202b211995585a7012
sorussmann <sofia.russmann@gmail.com>
2019-06-26 14:39:02 -0300

    carpetasyconsignas

---
+++ TP2-Ada/gley.js
@@ -0,0 +1,13 @@
+// TRABAJO PRÁCTICO NÚMERO 2
+// Integrantes: Gleyser y Sofía.
+// Ejercicios:
+//1- Funciones:
+//precioMaquina(componentes) SOFI
+// cantidadVentasComponente(componente): GLEY
+// vendedoraDelMes(mes, anio) usar precioMaquina SOFI 
+// ventasMes(mes, anio): GLEY
+// ventasVendedora(nombre): SOFI
+// componenteMasVendido(): usar cantidadVentasComponente GLEY
+// huboVentas(mes, anio): SOFI
+//2-
+//3-

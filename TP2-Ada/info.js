74263f88a8bb9ccd409231d8bc3a915745ed978f
sorussmann <sofia.russmann@gmail.com>
2019-07-03 00:41:26 -0300

    soficomienza2

--- TP2-Ada/info.js
+++ TP2-Ada/info.js
@@ -1,25 +1,25 @@
-var local = {
-    vendedoras: ["Ada", "Grace", "Hedy", "Sheryl"],
+const local = {
+  vendedoras: ["Ada", "Grace", "Hedy", "Sheryl"],
   
-    ventas: [
-      // tener en cuenta que Date guarda los meses del 0 (enero) al 11 (diciembre)
-      { fecha: new Date(2019, 1, 4), nombreVendedora: "Grace", componentes: ["Monitor GPRS 3000", "Motherboard ASUS 1500"] },
-      { fecha: new Date(2019, 0, 1), nombreVendedora: "Ada", componentes: ["Monitor GPRS 3000", "Motherboard ASUS 1500"] },
-      { fecha: new Date(2019, 0, 2), nombreVendedora: "Grace", componentes: ["Monitor ASC 543", "Motherboard MZI"] },
-      { fecha: new Date(2019, 0, 10), nombreVendedora: "Ada", componentes: ["Monitor ASC 543", "Motherboard ASUS 1200"] },
-      { fecha: new Date(2019, 0, 12), nombreVendedora: "Grace", componentes: ["Monitor GPRS 3000", "Motherboard ASUS 1200"] }
-    ],
+  ventas: [
+    // tener en cuenta que Date guarda los meses del 0 (enero) al 11 (diciembre)
+    { fecha: new Date(2019, 1, 4), nombreVendedora: "Grace", componentes: ["Monitor GPRS 3000", "Motherboard ASUS 1500"] },
+    { fecha: new Date(2019, 0, 1), nombreVendedora: "Ada", componentes: ["Monitor GPRS 3000", "Motherboard ASUS 1500"] },
+    { fecha: new Date(2019, 0, 2), nombreVendedora: "Grace", componentes: ["Monitor ASC 543", "Motherboard MZI"] },
+    { fecha: new Date(2019, 0, 10), nombreVendedora: "Ada", componentes: ["Monitor ASC 543", "Motherboard ASUS 1200"] },
+    { fecha: new Date(2019, 0, 12), nombreVendedora: "Grace", componentes: ["Monitor GPRS 3000", "Motherboard ASUS 1200"] }
+  ],
   
-    precios: [
-      { componente: "Monitor GPRS 3000", precio: 200 },
-      { componente: "Motherboard ASUS 1500", precio: 120 },
-      { componente: "Monitor ASC 543", precio: 250 },
-      { componente: "Motherboard ASUS 1200", precio: 100 },
-      { componente: "Motherboard MZI", precio: 30 },
-      { componente: "HDD Toyiva", precio: 90 },
-      { componente: "HDD Wezter Dishital", precio: 75 },
-      { componente: "RAM Quinston", precio: 110 },
-      { componente: "RAM Quinston Fury", precio: 230 }
-    ]
+  precios: [
+    { componente: "Monitor GPRS 3000", precio: 200 },
+    { componente: "Motherboard ASUS 1500", precio: 120 },
+    { componente: "Monitor ASC 543", precio: 250 },
+    { componente: "Motherboard ASUS 1200", precio: 100 },
+    { componente: "Motherboard MZI", precio: 30 },
+    { componente: "HDD Toyiva", precio: 90 },
+    { componente: "HDD Wezter Dishital", precio: 75 },
+    { componente: "RAM Quinston", precio: 110 },
+    { componente: "RAM Quinston Fury", precio: 230 }
+  ],
 };
 
